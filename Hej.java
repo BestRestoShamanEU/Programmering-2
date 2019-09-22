4c891aa587bf56b21ade362840f6cc0b1f943267
BestRestoShamanEU <42673669+BestRestoShamanEU@users.noreply.github.com>
2019-09-22 21:16:05 +0200

    .
    samma som förra

---
+++ Hej.java
@@ -0,0 +1,39 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Hej extends Application implements EventHandler<ActionEvent> {
+
+    Button buttonAdd;
+    Group root;
+    Scene mainScene;
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        buttonAdd = new Button("Add");
+        // buttonAdd.setText(jeff);
+
+        buttonAdd.setOnAction(this);
+
+        root = new Group(buttonAdd);
+
+        mainScene = new Scene(root); 
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        // TODO Auto-generated method stub
+        if (event.getSource() == buttonAdd;) 
+        { 
+            System.out.println("Du tryckte");
+        }
+    }
+}
